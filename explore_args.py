3f518f06578b07b231a8fb40d6a61315e64678c0
Junel Solis <junel.solis@abo.fi>
2022-05-09 17:50:30 +0300

    update explore script

--- explore_args.py
+++ explore_args.py
@@ -1,7 +1,8 @@
 import os
-from skimage import io, img_as_uint
+
+# from skimage import io, img_as_uint
 from skimage.filters import median, threshold_otsu
-from skimage.morphology import remove_small_objects
+from skimage.morphology import remove_small_objects, binary_opening, binary_closing
 import scipy.ndimage as ndi
 import napari
 import numpy as np
@@ -9,7 +10,7 @@ import cv2
 
 viewer = napari.Viewer()
 
-img = cv2.imread("arg/DPX71.tif", cv2.IMREAD_GRAYSCALE)
+img = cv2.imread("arg/DPX91.tif", cv2.IMREAD_GRAYSCALE)
 
 viewer.add_image(img, name="DPX71.tif", colormap="magma")
 
@@ -23,7 +24,9 @@ thresholded = blurred > threshold_value
 viewer.add_image(thresholded, name="Otsu threshold")
 
 fill_holes = ndi.binary_fill_holes(thresholded)
+fill_holes = binary_opening(fill_holes, np.ones((5, 5)))
+# fill_holes = binary_closing(fill_holes, np.ones((5, 5)))
 viewer.add_image(fill_holes, name="fill_holes")
 
-large_objects_only = remove_small_objects(fill_holes, min_size=2000)
+large_objects_only = remove_small_objects(fill_holes, min_size=5000)
 viewer.add_image(large_objects_only, name="large_objects_only")
