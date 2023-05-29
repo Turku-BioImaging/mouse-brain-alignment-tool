9864756f150bc2de4fb62a6e902812963b3c9c47
Junel Solis <junel.solis@abo.fi>
2023-05-29 14:54:48 +0300

    workling preprocessing ui

--- src/setup_ui.py
+++ src/setup_ui.py
@@ -1,60 +1,116 @@
-import sys
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
-from PyQt5.uic import loadUi
+import os
 import subprocess
 
+from PyQt5.QtWidgets import QApplication, QDialog, QFileDialog
+from PyQt5.uic import loadUi
+
+
 class MainWindow(QDialog):
+    num_slides = 1
+    num_animals = 1
+    image_fname = ""
+    output_dir = ""
+    animal_left_name = ""
+    animal_right_name = ""
+
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi("C:/Users/zuzka/Desktop/Finland_2022/ARR/code/qt_guis/preprocess.ui", self)
-        self.browse.clicked.connect(self.browsefolder)
-        self.slidecount()
-        self.animalcount()
-        self.slides_1.clicked.connect(self.slidecount)
-        self.slides_2.clicked.connect(self.slidecount)
-        self.animals_1.clicked.connect(self.animalcount)
-        self.animals_2.clicked.connect(self.animalcount)
+        loadUi(os.path.join(os.path.dirname(__file__), "gui", "preprocess.ui"), self)
+
+        self.select_image.clicked.connect(self.set_image_fname)
+        self.set_slide_count()
+        self.num_slides_1.clicked.connect(self.set_slide_count)
+        self.num_slides_2.clicked.connect(self.set_slide_count)
+
+        self.set_animal_count()
+        self.num_animals_1.clicked.connect(self.set_animal_count)
+        self.num_animals_2.clicked.connect(self.set_animal_count)
+
+        self.animal_left_name_textbox.textChanged.connect(self.set_animal_left_name)
+        self.animal_right_name_textbox.textChanged.connect(self.set_animal_right_name)
+
+        self.select_output_directory.clicked.connect(self.set_output_dir)
+
         self.process.clicked.connect(self.run)
 
-    def slidecount(self):
-        global nslides
-        nslides = "2"
-        self.group_animals.setEnabled(True)
-        self.animal_names.setEnabled(True)
-        if self.slides_1.isChecked() is True:
-            nslides = "1"
-            self.group_animals.setEnabled(False)
-            self.animal_names.setEnabled(False)
+    def set_slide_count(self):
+        if self.num_slides_1.isChecked() is True:
+            self.num_slides = 1
+        if self.num_slides_2.isChecked() is True:
+            self.num_slides = 2
 
-    def animalcount(self):
-        global nanimals
-        nanimals = "2"
-        self.animal_names.setEnabled(True)
-        if self.animals_1.isChecked() is True:
-            nanimals = "1"
-            self.animal_names.setEnabled(False)
+        self.set_animal_count()
 
-    def browsefolder(self):
-        global fname
-        fname = QFileDialog.getExistingDirectory(self, "Select Directory", "C:/Users/zuzka/Desktop/Finland_2022/ARR")
-        self.foldername.setText(fname)
-        print(fname)
+    def set_animal_count(self):
+        if self.num_animals_1.isChecked() is True:
+            self.num_animals = 1
+            self.group_animal_names.setEnabled(False)
+        if self.num_animals_2.isChecked() is True:
+            self.num_animals = 2
+            self.group_animal_names.setEnabled(True)
+
+    def set_animal_left_name(self):
+        self.animal_left_name = self.animal_left_name_textbox.text()
+
+    def set_animal_right_name(self):
+        self.animal_right_name = self.animal_right_name_textbox.text()
+
+    def set_image_fname(self):
+        self.image_fname, _ = QFileDialog.getOpenFileName(self, "Select File")
+        self.image_fname_textbox.setText(self.image_fname)
+
+    def set_output_dir(self):
+        self.output_dir = QFileDialog.getExistingDirectory(self, "Select Directory")
+        self.output_directory_textbox.setText(self.output_dir)
 
     def run(self):
-        nameL, nameR = self.animal_left.text(), self.animal_right.text()        
-        if self.animal_names.isEnabled() is True:
-            nanimals = "2"
-            subprocess.call("python explore_args_Z.py %s %s %s %s %s" %(fname, nslides, nanimals, nameR, nameL), shell=True)
-        else:
-            nanimals = "1"
-            subprocess.call("python explore_args_Z.py %s %s %s" %(fname, nslides, nanimals), shell=True)
+        script_path = os.path.join(
+            os.path.dirname(__file__), "modules", "preprocessing.py"
+        )
 
-app = QApplication(sys.argv)
-mainwindow = MainWindow()
-widget = QtWidgets.QStackedWidget()
-widget.addWidget(mainwindow)
-widget.setFixedWidth(700)
-widget.setFixedHeight(500)
-widget.show()
-sys.exit(app.exec_())
+        if self.num_animals == 2:
+            subprocess.call(
+                [
+                    "python",
+                    script_path,
+                    "--image-path",
+                    self.image_fname,
+                    "--num-slides",
+                    str(self.num_slides),
+                    "--num-animals",
+                    str(self.num_animals),
+                    "--animal-left-name",
+                    self.animal_left_name,
+                    "--animal-right-name",
+                    self.animal_right_name,
+                    '--output-dir',
+                    self.output_dir
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+
+        if self.num_animals == 1:
+            subprocess.call(
+                [
+                    "python",
+                    script_path,
+                    "--image-path",
+                    self.image_fname,
+                    "--num-slides",
+                    str(self.num_slides),
+                    "--num-animals",
+                    str(self.num_animals),
+                    "--output-dir",
+                    self.output_dir,
+                ],
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+            )
+
+
+if __name__ == "__main__":
+    app = QApplication([])
+    window = MainWindow()
+    window.show()
+    app.exec_()
