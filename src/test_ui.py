ef721e61b7291f79a7f73f36884ae0b4b7f565e7
Junel Solis <junel.solis@abo.fi>
2023-05-29 12:15:41 +0300

    Squashed commit of the following:
    commit 37d29cef4216c4761c0a3c919028442af0d76d44
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon May 29 12:13:08 2023 +0300
    
        move processed files to selected output dir
    
    commit daa56e3a1994bdc4467cd7a665dc69ed6ea658ff
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon May 29 11:58:36 2023 +0300
    
        save tiff
    
    commit ba729ec817f04ed5579d243af1694906f3960bb0
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon May 29 11:46:28 2023 +0300
    
        saves sections for different combinations of slides and animals
    
    commit 1c93ecb07b9301528f0f7e388e42bd426bb9b663
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon May 29 11:35:17 2023 +0300
    
        saves sections with multiple animals
    
    commit e95de8229f7fd44d9a88725bc43cbdb89d16ffc4
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon May 29 11:34:46 2023 +0300
    
        ignore workdirs
    
    commit c09f6136f6972a0f6700e861ab9ddf0643eb45fa
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon May 29 10:31:53 2023 +0300
    
        preprocessing module for creating sections
    
    commit 40f6d161bd22b59e95686f97b6eb6c13a0069839
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Wed Feb 8 12:17:30 2023 +0200
    
        use rows instead of slides terminology
    
    commit 23953ea3f89284d331bb6b52e8ec312e1da1bfd6
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Tue Feb 7 16:34:24 2023 +0200
    
        add preprocess ui changes WIP
    
    commit 000efe100ab97af4884c09e6a076d1edc8bfd3f1
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Tue Feb 7 10:11:04 2023 +0200
    
        ignore data dir
    
    commit 81dcc3474f5aa791da737802e144e7c4f4a01525
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Tue Feb 7 09:40:20 2023 +0200
    
        depend matplotlib
    
    commit 0ddad0b95779660e877b9146cf3d617ce898c2b0
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Tue Feb 7 09:40:09 2023 +0200
    
        move file location
    
    commit 8680532bf0fad53e812aae90a81a88e398ddba51
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon Feb 6 10:47:19 2023 +0200
    
        test WIP
    
    commit 1531136b66062176e58e08f7d2952f0d2bde198e
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon Feb 6 09:55:48 2023 +0200
    
        test ui WIP
    
    commit fde5e7679a66b7caa225f41dc9959237fd1aff3c
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon Feb 6 09:42:35 2023 +0200
    
        move files around, add pyqt5 dependency, WIP
    
    commit 5962505748559b0457d29f26b442799ead00b389
    Author: Junel Solis <junel.solis@abo.fi>
    Date:   Mon Feb 6 09:22:48 2023 +0200
    
        rename module

--- src/test_ui.py
+++ src/test_ui.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 from PyQt5.QtCore import QSize, Qt
 from PyQt5.QtWidgets import (
     QApplication,
@@ -46,3 +47,77 @@ app = QApplication(sys.argv)
 window = MainWindow()
 # window.show()
 app.exec()
+=======
+import sys
+import os
+from PyQt5.QtWidgets import QDialog, QApplication, QFileDialog
+from PyQt5.uic import loadUi
+import subprocess
+
+
+class MainWindow(QDialog):
+    def __init__(self):
+        super(MainWindow, self).__init__()
+        loadUi(os.path.join("qt_gui", "preprocess.ui"), self)
+        self.browse.clicked.connect(self.browse_folder)
+        self.row_count()
+        self.animal_count()
+        self.rows_1.clicked.connect(self.row_count)
+        self.rows_2.clicked.connect(self.row_count)
+        self.animals_1.clicked.connect(self.animal_count)
+        self.animals_2.clicked.connect(self.animal_count)
+        self.process.clicked.connect(self.run)
+
+    def row_count(self):
+        global n_rows
+        n_rows = "2"
+        self.group_animals.setEnabled(True)
+        self.animal_names.setEnabled(True)
+        if self.rows_1.isChecked() is True:
+            n_rows = "1"
+            self.group_animals.setEnabled(False)
+            self.animal_names.setEnabled(False)
+
+    def animal_count(self):
+        global n_animals
+        n_animals = 2
+        self.animal_names.setEnabled(True)
+        if self.animals_1.isChecked() is True:
+            n_animals = 1
+            self.animal_names.setEnabled(False)
+
+    def browse_folder(self):
+        global fname
+        fname = os.path.join(
+            QFileDialog.getExistingDirectory(
+                self,
+                "Select Directory",
+                os.path.join(os.getcwd(), "data"),
+            ),
+            "",
+        )
+
+        self.foldername.setText(fname)
+
+    def run(self):
+        name_bottom, name_top = self.animal_left.text(), self.animal_right.text()
+        
+        if self.animal_names.isEnabled() is True:
+            n_animals = "2"
+            subprocess.call(
+                f"python preprocess_slides.py {fname} {n_rows} {n_animals} {name_top} {name_bottom}",
+                shell=True,
+            )
+        else:
+            n_animals = "1"
+            subprocess.call(
+                f"python preprocess_slides.py {fname} {n_rows} {n_animals}", shell=True
+            )
+
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    app.exec()
+>>>>>>> refactor-ui
